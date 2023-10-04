542324f0772e8d64b845731c8187c47c85aa88c7
Sebas J <juan.jaramillo0622@gmail.com>
2023-10-04 11:35:12 -0500

    Ejercicio de clase 03-10-23

---
+++ src/Ejercicioclase.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+
+import javax.swing.JOptionPane;
+
+public class Ejercicioclase {
+    public static void main(String[] args) throws Exception {
+        
+    String dato = JOptionPane.showInputDialog("¿De que tamaño quieres tu arreglo?"); 
+    int n = Integer.parseInt(dato); 
+    Integer arreglo [] = new Integer[n];
+    
+    for (int i = 0; i < n; i++) {
+        int num; 
+        num = (int)Math.round(Math.random()*100); 
+        arreglo[i] = num; 
+        System.out.println("Los datos que tienes son: " + arreglo[i]);
+        System.out.println("_________________________");
+        
+    }
+    {
+    Arrays.sort(arreglo);
+    for (int s : arreglo) 
+    System.out.println("Tus datos de menor a mayor son: " +s);
+    System.out.println("_________________________"); }
+        {
+    Arrays.sort(arreglo, Collections.reverseOrder());
+    for (int s : arreglo) 
+    System.out.println("Tus datos de mayor a menor son: " +s);
+    System.out.println("_________________________");
+        
+    
+
+
+                
+}
+            
+}
+}
+        
+            
+            
+        
+        
+
+
+
+        
+
+
+        
+        
+
+        
+
+
+
+
+
+
+    
+
